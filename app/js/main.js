1c67d20bec8bb06ba026a7dc15cd6c847ba4a8a6
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-04-10 21:22:55 +0500

    заякорил ссылки

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+"use string"
+
 $(function () {
     const element = document.querySelector("#wrap");
 
@@ -10,10 +12,22 @@ $(function () {
         });
     });
 
-    $("body").on('click', '[href*="#"]', function(e){
-        var fixed_offset = 100;
-        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
-        e.preventDefault();
-      });
+
+    $(".menu").on("click", "a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            left = $(id).offset().left;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollLeft: left }, 1500);
+    });
+
+
 });
+
 
